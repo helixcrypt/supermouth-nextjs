4e9ad04f59840a7bf6b6246bcfc5e8ec290e9656
Gregory Patterson <g@helixcrypt.com>
2026-06-27 09:59:07 -0700

    Disable image cache TTL to fix stale 404 on tamronhall.jpg

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    minimumCacheTTL: 0,
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.supermouth.com' },
       { protocol: 'https', hostname: 'supermouthcdn.blob.core.windows.net' },
       { protocol: 'https', hostname: 'cdn.shopify.com' },
       { protocol: 'https', hostname: 'pics.io' },
-      { protocol: 'https', hostname: 'cdn.mos.cms.futurecdn.net' },
-      { protocol: 'https', hostname: 'img.huffingtonpost.com' },
-      { protocol: 'https', hostname: 'camillestyles.com' },
     ],
   },
 }
